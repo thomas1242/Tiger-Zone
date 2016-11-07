a9544094fdffb611f99d60fd26e5e17a6dfd8d9a
kurtyilmaz <mistahkurtyilmaz@gmail.com>
2016-11-07 13:41:28 -0500

    Working on Board class

--- src/board.cpp
+++ src/board.cpp
@@ -18,28 +18,28 @@ Board::Board() {
 // Mark available spots for the board
 void Board::markavail(int xcoord, int ycoord) {
 	//Marking space above
-	if(xcoord > 0) {
+	if(xcoord > 0 && matrix[xcoord-1][ycoord] == 0) {
 		//check to see if card is already there, discuss what to do
 		matrix[xcoord-1][ycoord] = new card(0);
 		matrix[xcoord-1][ycoord]->avail_bot = matrix[xcoord][ycoord]->top;
 	}	
 
 	//Marking space below
-	if(xcoord < boardlength) {
+	if(xcoord < boardsize && matrix[xcoord+1][ycoord] == 0) {
 		//check to see if card is already there, discuss what to do
 		matrix[xcoord+1][ycoord] = new card(0);
 		matrix[xcoord+1][ycoord]->avail_top = matrix[xcoord][ycoord]->bot;
 	}
 	
 	//Marking space on left
-	if(ycoord > 0) {
+	if(ycoord > 0 && matrix[xcoord][ycoord-1] == 0) {
 		//check to see if card is already there, discuss what to do
 		matrix[xcoord][ycoord-1] = new card(0);
 		matrix[xcoord][ycoord-1]->avail_right = matrix[xcoord][ycoord]->left;
 	}
 	
 	//Marking space on right
-	if(ycoord < boardlength) {
+	if(ycoord < boardsize && matrix[xcoord][ycoord+1] == 0) {
 		//check to see if card is already there, discuss what to do
 		matrix[xcoord][ycoord+1] = new card(0);
 		matrix[xcoord][ycoord+1]->avail_left = matrix[xcoord][ycoord]->right;
@@ -55,16 +55,18 @@ bool Board::place(int xcoord, int ycoord, Card input) {
 	if(checkspace[xcoord][ycoord] == true) {
 		//Place it
 	}
-	else {
+	else { 
 		//Don't do anything, return false
 	}
 }
 
+//Konami Kode Top, Bot, Left, Right
 void Board::printBoard(){
     cout << endl;
-    for(int i = 0; i < boardlength; i++){
-        for(int j = 0; j < boardlength; j++){
-            printf ("%3d ", board[i][j]);
+    for(int i = 0; i < boardsize && ; i++){
+        for(int j = 0; j < boardsize && ; j++){
+            printf ("%3d ", matrix[i][j]->top, matrix[i][j]->bot,
+            		matrix[i][j]->left, matrix[i][j]->right);
         }
         cout << endl;
     }
--- src/card.cpp
+++ src/card.cpp
@@ -3,10 +3,17 @@
 
 Card::card(int x) {
 	id = x;
-	avail_top = 0;
-	avail_bot = 0;
-	avail_right = 0;
-	avail_left = 0;
+	avail_top = '0';
+	avail_bot = '0';
+	avail_right = '0';
+	avail_left = '0';
+}
+
+void Card::assignsides(char t, char b, char l, char r) {
+	top = t;
+	bot = b;
+	left = l;
+	right = r;
 }
 
 void Card::rotatecw() {
