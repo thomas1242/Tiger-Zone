6d03c25c8f83372eb58c30868c70af6819e24417
thomas1242 <thomaspalmer121@gmail.com>
2016-11-08 02:24:22 -0500

    Update deck.cpp

--- src/deck.cpp
+++ src/deck.cpp
@@ -1,46 +1,57 @@
-#include "main.h"
-#include "board.h"
 #include <stdlib.h>     /* srand, rand */
-#include <time.h>
 #include <string>
+#include <stack>
+#include <iostream>
+#include <sstream>
+#include "deck.h"
+using namespace std;
 
-void randomseed() {
-	srand (time(NULL));             			
-	int card;
-	for(int i = 0; i < decksize - 1; i++) {   // put 71 cards into the deck
-	   
-	    do{
-	        card = rand() % 23 + 1;                             // generate random card
-	    } while ( counts[ card ] >= maxCounts[  card ] );       // only use cards we need
-	    
-	    deck.push( card );
-	    counts[ card ]++;
-	    cards_in_deck++;
-	}
-}
-
+// Deck constructor
 Deck::Deck() {
-	// initally 0 of each card
-	for(int i = 0; i < uniquecards; i++) {
-    	counts[i] = 0;
-	}
-}
-
-static inline string int2Str(int x) {
-    std::stringstream type;
-    type << x;
-    return type.str();
-}
-
-void popCardOffDeck() {                // pop top card off the deck
-    if( deck.size() != 0) {
-        deck.pop();
-        cards_in_deck--;
+    numCardsInDeck = 0;
+    for(int i = 0; i < UNIQUECARDS; i++) {	  // initally 0 cards in the deck
+        counts[i] = 0;
+    }
+    int maxCounts[] = { 3, 1, 2, 3, 2, 3, 3, 8, 5, 1, 9, 3, 3, 4, 3, 3, 1, 4, 1, 1, 2, 2, 2, 2 };
+    srand (time(NULL));
+    int card;
+    for(int i = 0; i < MAXCARDS; i++) {   // put 71 cards into the deck
+        do{
+            card = rand() % 24;                                // generate random card
+        } while ( counts[ card ] >= maxCounts[ card ] );       // only use cards we need
+        
+        deck.push( card );	// put the card in the deck
+        counts[ card ]++;
+        numCardsInDeck++;
     }
 }
 
-string getDeckString() {
-    std::ostringstream oss;
-	oss << cards_in_deck;
+// pop top card off the deck
+void Deck::popCardOffDeck() {
+    if( deck.size() != 0) {
+        deck.pop();
+        numCardsInDeck--;
+    }
+}
+
+// convert int to string to create score string
+string Deck::getDeckString() {
+    ostringstream oss;
+    oss << numCardsInDeck;
     string s = "cards left: " + oss.str();
     return s;
+}
+
+// print number of each type of card currently in the deck
+void Deck::printDeck() {
+    int sum = 0;
+    for(int i = 0; i < UNIQUECARDS; i++) {
+        cout << "# of " << i << "'s = " << counts[i] << '.' << endl;
+        sum += counts[i];
+    }
+    cout << "total number of cards in the deck = " << sum << endl;
+    cout << "total number of cards in the deck = " << numCardsInDeck << endl;
+}
+
+
+
