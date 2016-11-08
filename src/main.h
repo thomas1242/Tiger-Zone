8df48ee1b668a27a31e890f78dfe4dd9c8008655
thomas1242 <thomaspalmer121@gmail.com>
2016-11-08 02:24:49 -0500

    Delete main.h

--- src/main.h
+++
@@ -1,9 +0,0 @@
-// Putting stuff in here. Can be put in card.h, deck.h etc later.
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <stack>
-
-#define decksize 72						// Including first card in size
-
