838929ca02ecaf0525758fa2432fdfadf9a70849
thomas1242 <thomaspalmer121@gmail.com>
2016-11-08 02:25:53 -0500

    Update deck.h

--- src/deck.h
+++ src/deck.h
@@ -1,29 +1,27 @@
-#include "main.h"
-#ifndef Deck_h
-#define Deck_h
-#include <string>
+#ifndef deck_h
+#define deck_h
 
-#define uniquecards 25					// Number of unique cards;
+#include <stdio.h>
+#include <string>
+#include <stack>
+#define UNIQUECARDS 24        		// number of unique cards in the deck
+#define MAXCARDS    71        		// max number of cards allowed in the deck
+using namespace std;
 
 class Deck {
-
-public:
-	int cards_in_deck = 0;			// cards currently in the deck
-	
-	// array to hold count of each card in the deck
-	int counts[uniquecards];
-	
-	// HARDCODED VALUES FOR CARCASSONNE ONLY. EDIT IF CARDS CHANGE
-	// 24 unique cards at indices 1-24, fixed number of each, 72 cards total.    
-	int maxCounts[] = { 0, 3, 1, 2, 3, 2, 3, 3, 8, 5, 1, 9, 3, 3, 4, 3, 3, 1, 4, 1, 1, 2, 2, 2, 2};
-
-	static inline string int2Str(int x);
-	void popCardOffDeck();
-	std::string getDeckString();
-
-private:
-
-
+    
+    public:
+        Deck();							// Deck constructor
+        ~Deck();
+        string getDeckString();			// return string with num cards left in deck (for the GUI)
+        void popCardOffDeck();			// pop top card off the deck
+        void printDeck();				// for debugging:print count of each card currently in the deck
+        
+    private:
+        int numCardsInDeck;			    // total nubmer of cards currently in the deck
+        int counts[UNIQUECARDS];	    // count of each card currently in the deck
+        stack< int > deck;              // stack of card ids
+        
 };
 
-#endif
+#endif /* deck_h */
