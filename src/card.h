f3ec2bb46cba692bb2a6dd08d0b3d911e4b48e83
thomas1242 <thomaspalmer121@gmail.com>
2016-11-08 02:44:12 -0500

    Update card.h

--- src/card.h
+++ src/card.h
@@ -14,7 +14,7 @@ class Card {
         char top, bot, left, right;     // c = city, r = road, f= field
         
     public:
-        Card();             // construct 'null' card with id = -1 
+        Card();             // construct 'null' card with id = -1 (free space card)
         Card(int id);       // construct a Card a.k.a. an instance of a tile from the deck
         void rotate();      // rotate card 90 degress clockwise
         void assignSides(int id);
