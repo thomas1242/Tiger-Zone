3f7026d968bdf6c751ab492438448de886bd532f
drace128 <drace128@gmail.com>
2016-11-18 00:47:02 -0500

    These are just barebones files for the server socket to be later filled

---
+++ src/server.h
@@ -0,0 +1,13 @@
+#ifndef server_h
+#define server_h
+#include <iostream>
+
+using namespace std;
+//much more will be added
+class Server{
+public:
+	Server();
+	void createSocket();
+};
+
+#endif //server_h
