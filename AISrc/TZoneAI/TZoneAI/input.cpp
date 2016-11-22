14a5f7a76e3fd57b90c22ada52a4ec8777517ef6
EvanMAmstutz <evangolf1@ufl.edu>
2016-11-22 15:42:15 -0500

    Develop Branch init
    initial develop branch commit

---
+++ AISrc/TZoneAI/TZoneAI/input.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  TZoneAI
+//
+//  Created by Evan Amstutz on 11/22/16.
+//  Copyright © 2016 Evan Amstutz. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Input{
+    
+    
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "test!\n";
+    return 0;
+}
---
+++ AISrc/TZoneAI/TZoneAI/output.cpp
@@ -0,0 +1,9 @@
+//
+//  output.cpp
+//  TZoneAI
+//
+//  Created by Evan Amstutz on 11/22/16.
+//  Copyright © 2016 Evan Amstutz. All rights reserved.
+//
+
+#include "output.hpp"
