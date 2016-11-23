d53413324729c45080ab4614cfd219d0b9e08004
kurtyilmaz <mistahkurtyilmaz@gmail.com>
2016-11-23 18:10:48 -0500

    Fixed compiler errors of new Moves implementation from Thomas and I. Logic and testing still needs done.

--- GameSrc/card.h
+++ GameSrc/card.h
@@ -9,41 +9,37 @@
 
 class Card {
     
-private:
-    int id;                         // card identifier: what type of tile is this
-    char top, bot, left, right;     // c = city, r = road, f= field
-    char top_L, top_R, bot_L, bot_R;
-    char mid;
-    int orientation;    // degreees rotated clockwise, initally 0
+    private:
+        int id;                         	            // card identifier: what type of tile is this
+        int orientation;                                // degreees rotated clockwise, initally 0  
+        bool isden;                                     // states card is a den
+        bool endtrail;                                  // states card ends trail
+        char top, bot, left, right;     	            // l = lake, g = game-trail, j = jungle  
+/*        char T_L, T_R, B_L, B_R;                      // corner tiles
+        char mid;
+*/
     
-    
-public:
-    Card();             // construct null card ( id = 0 ). need this constructor for our board constructor
-    Card(int id);       // construct a Card a.k.a. an instance of a tile from the deck
-    void rotate();      // rotate card 90 degress clockwise
-    void assignSides(int id);
-    void printCard();
-    
-    char getTop();  // functions so other classes can have controlled access to private variables
-    char getBot();
-    char getRight();
-    char getLeft();
-    char getTop_L();
-    char getTop_R();
-    char getBot_L();
-    char getBot_R();
-    char getMid();
-    
-    
-    int getId();
-    int getOrient( );
-    
-    
-    int top_RoadID;     // what feature each segment belongs to
-    int bot_RoadID;     // what feature each segment belongs to
-    int left_RoadID;    // what feature each segment belongs to
-    int right_RoadID;   // what feature each segment belongs to
-    int mid_RoadID;     // what feature each segment belongs to
+    public:
+        Card();             // construct null card ( id = 0 ). need this constructor for our board constructor
+        Card(int id);       // construct a Card a.k.a. an instance of a tile from the deck
+        void rotate();      // rotate card 90 degress clockwise
+        void assignSides(int id);
+        void printCard();
+        
+        int getId();
+        int getOrient( );
+        int getEndtrail();
+        char getTop();  // functions so other classes can have controlled access to private variables
+        char getBot();
+        char getRight();
+        char getLeft();
+/*        
+        char getT_L();
+        char getT_R();
+        char getB_L();
+        char getB_R();
+        char getMid();
+*/
     
 };
 
