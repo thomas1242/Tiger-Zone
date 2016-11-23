5b5940ca3a4a68157154ff6f169838f0080f0ba0
kurtyilmaz <mistahkurtyilmaz@gmail.com>
2016-11-23 18:08:03 -0500

    Fixed compiler errors of new Moves implementation from Thomas and I. Logic and testing still needs done.

--- GameSrc/board.cpp
+++ GameSrc/board.cpp
@@ -4,66 +4,92 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define TOPLEFT     1
-#define TOPCENTER   2
-#define TOPRIGHT    3
-#define LEFT        4
-#define MID         5
-#define RIGHT       6
-#define BOTLEFT     7
-#define BOTCENTER   8
-#define BOTRIGHT    9
-
+#include <list>
 using namespace std;
 
-Board::Board(Card * card) {                            // board constructor
-    
-    board = new Card * [ROWS];              // board is initially all open space ( cards with id = -1 )
-    for(int i = 0; i < ROWS; ++i) {
-        board[i] = new Card[COLS];
-        for(int j = 0; j < COLS; j++) {
-            possibleMoves[i][j] = false;    // initalize possible moves
-        }
-    }
-    
-    cout << "Put center card on new board" << endl;
-    possibleMoves[ROWS/2][COLS/2] = true;                        // mark center location available
-    placeCard(ROWS/2, COLS/2, card );        // place center card on the board
-    
+Coords::Coords(int i, int j) {
+	icoord = i;
+	jcoord = j;
 }
 
+Moves::Moves(int i, int j) {
+    icoord = i;
+    jcoord = j;
+    for (int i = 0; i < 4; i++) {
+        possibleorientations[i] == 0;
+    }
+}
 
-bool Board::placeCard(int i, int j, Card * card) {
+Board::Board(Card * card) {                        // board constructor
+    
+    board = new Card * [ROWS];              // board is initially all open space ( cards with id = -1 )
+
+    for(int i = 0; i < ROWS; ++i) {
+        board[i] = new Card[COLS];
+    }
+
+    cout << "Put center card on new board" << endl;
+    Moves temp(ROWS/2, COLS/2);
+    possibleMoves.push_back(temp);
+    bool result = placeCard(ROWS/2, COLS/2, card );        // place center card on the board
+}
+    
+/*******************************************************************************************
+    needs input argument to specify rotation
+********************************************************************************************/
+bool Board::placeCard(int i, int j, Card* card) {
     
     if(i < 0 || j < 0 || i >= ROWS || j >= COLS) {  // for GUI, if user clicks out of bounds
-        cout << "Cannot place card " << card->getId() << " at (" << i << ',' << j << ']' << endl;
         return false;
     }
     
     // before placing this card on the board, make sure the location is valid
-    if( !possibleMoves[i][j] ) {
-        cout << "Cannot place card " << card->getId() << " at (" << i << ',' << j << ']' << endl;
-        return false;
+    if( board[i][j].getId() != -1 ) {
+       cout << "Cannot place card " << card->getId() << " at (" << i << ',' << j << ']' << endl;
+       return false;
     }
-    
-    while( !checkIfFits(i, j, card) ) {
+
+// Match it with avail side
+/*    
+   while( !checkIfFits(i, j, card) ) {
         card->rotate();                     // rotate card until it fits at the valid location
     }
-    
-    cout << "Placed card " << card->getId() << " at [" << i << ',' << j << ']' << endl;
-    board[i][j] = *card;            // replace with new card
+*/            
+    else {
+        cout << "Placed card " << card->getId() << " at [" << i << ',' << j << ']' << endl;
+        board[i][j] = *card;            // replace with new card
+        markavail(i, j, card);
         
-    // rotate option
-    // place meeple()
-    //printBoard();
-    return true;
+        printBoard();
+        return true;
+    }
+}
+    
+
+bool Board::updatePossibleMoves(Card * card) { // possible moves based on board state and current card
+	Moves temp(markedtiles.back().icoord, markedtiles.back().jcoord);
+	bool works = false;
+	for(int i = 0; i < 4; i++) {
+		if(checkIfFits(markedtiles.back().icoord, markedtiles.back().jcoord, card) == true) {
+			temp.possibleorientations[i] = 1;
+			works = true;
+		}
+		card->rotate();
+	}
+
+	if(works == false) {
+		return false;
+	}
+
+	else {
+		possibleMoves.push_back(temp);
+	}
 }
 
-
+/*
 void Board::updatePossibleMoves(Card * card) { // possible moves based on board state and current card
     
-    for(int i = 0; i < ROWS; i++) {
+    for(int i = 0; i < ROWS; i++) {     
         for(int j = 0; j < COLS; j++) {
             if(board[i][j].getId() == -1) {                                  // if this spot is not taken (open space)
                 if(i - 1 > 0 && board[i - 1][j].getId() != -1) {             // and is adjacent to a taken spot
@@ -87,76 +113,89 @@ void Board::updatePossibleMoves(Card * card) { // possible moves based on board 
     
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLS; j++) {
-            if( possibleMoves[i][j] == true ) { // if this spot is potentially valid (it's adjacent to another tile)
-                int n = 1;
-                while(n <= 4) {                          // 0, 90, 180, 270
-                    if(checkIfFits(i, j, card)) {
-                        break;
-                    }
-                    card->rotate();
-                    n++;
-                }
-                if( n > 4) {
-                    possibleMoves[i][j] = false;;
-                }
-                else {
-                    possibleMoves[i][j] = true;
-                }
-            }
-        }
-    }
-    
-}
+            if( possibleMoves[i][j] == true ) { // if this spot is potentially valid (it's a
+*/  
 
 bool Board::checkIfFits(int i, int j, Card * card ) {   // i is row, j is col
     
     bool result = true;
-    
     // check that the sides match OR are next to open space
-    
     // there is no tile above, tile above matches the top, tile above is open space
-    if( !(i == 0 || card->getTop() == board[i-1][j].getBot() || board[i-1][j].getBot() == 'o' ) ) {              // check top side
+    if(i == 0 || card->getTop() == board[i-1][j].getBot() || board[i-1][j].getBot() == 'o') {              // check top side
         result = false;
     }
     // there is no tile below, tile below matches the bot, tile below is open space
-    if( !(i == ROWS - 1 || card->getBot() == board[i+1][j].getTop() || board[i+1][j].getTop() == 'o' ) ) {         // check bot side
+    if(i == ROWS - 1 || card->getBot() == board[i+1][j].getTop() || board[i+1][j].getTop() == 'o') {         // check bot side
         result = false;
     }
     // there is no tile on right, tile on right matches the right, right tile is open space
-    if( !(j == COLS - 1 || card->getRight() == board[i][j+1].getLeft() || board[i][j+1].getLeft() == 'o' ) ) {         // check right side
+    if(j == COLS - 1 || card->getRight() == board[i][j+1].getLeft() || board[i][j+1].getLeft() == 'o') {         // check right side
         result = false;
     }
     // there is no tile on left, tile on left matches the left, left tile is open space
-    if( !(j == 0 || card->getLeft() == board[i][j-1].getRight() || board[i][j-1].getRight() == 'o' ) ) {         // check left side
+    if(j == 0 || card->getLeft() == board[i][j-1].getRight() || board[i][j-1].getRight() == 'o') {         // check left side
         result = false;
     }
-    
+
     return result;
 }
 
+// Mark available spots for the board
+void Board::markavail(int i, int j, Card* card) {
+	//Marking space above
+	if(i > 0) {
+        //check to see if card is already there
+        if(board[i-1][j].getId() == -1) {
+            Coords temp(i-1,j);
+            markedtiles.push_back(temp);
+        }			
+	}	
 
-
-
-
-
+	//Marking space below
+	if(i < ROWS-1) {
+        //check to see if card is already there
+        if(board[i+1][j].getId() == -1) {
+            Coords temp(i+1,j);
+            markedtiles.push_back(temp);
+        }
+	}
+	
+	//Marking space on left
+	if(j > 0) {
+        //check to see if card is already there
+        if(board[i][j-1].getId() == -1) {
+            Coords temp(i,j-1);
+            markedtiles.push_back(temp);
+        }
+	}
+	
+	//Marking space on right
+	if(j < COLS-1) { 
+        //check to see if card is already there
+        if(board[i][j+1].getId() == -1) {
+            Coords temp(i,j+1);
+            markedtiles.push_back(temp);
+        }
+	}	
+}
 
 
 /*  TL  TOP  TR
- ML  MID  MR
- BL  BOT  BR   */
+    ML  MID  MR
+    BL  BOT  BR   */
 void Board::printBoard() {
-    cout << "\nTHE BOARD: " << endl;
-    for(int i = 0; i < ROWS; i++) {           // for each row
-        for(int n = 0; n < 3; n++) {        // 3 'rows' per row
-            for(int j = 0; j < COLS; j++) {   // for each col
+  /*  cout << "\nTHE BOARD: " << endl;
+    for(int i = 0; i < ROWS; i++) {           	// for each row
+        for(int n = 0; n < 3; n++) {        	// 3 'rows' per row
+            for(int j = 0; j < COLS; j++) {   	// for each col
                 if(n == 0) {
-                    cout << board[i][j].getTop_L() << ' ' << board[i][j].getTop() << ' ' << board[i][j].getTop_R() << ' ';
+                    cout << board[i][j].getT_L() << ' ' << board[i][j].getTop() << ' ' << board[i][j].getT_R() << ' ';
                 }
                 else if (n == 1) {
                     cout << board[i][j].getLeft() << ' ' << board[i][j].getMid() << ' ' << board[i][j].getRight() << ' ';
                 }
                 else if (n == 2) {
-                    cout << board[i][j].getBot_L() << ' ' << board[i][j].getBot() << ' ' << board[i][j].getBot_R() << ' ';
+                    cout << board[i][j].getB_L() << ' ' << board[i][j].getBot() << ' ' << board[i][j].getB_R() << ' ';
                 }
                 cout << ' ';
             }
@@ -165,11 +204,11 @@ void Board::printBoard() {
                     cout << "  " << board[i][k].getId() << "  ";
                 }
             }
-            cout << endl;
+           cout << endl;
         }
         cout << endl;
     }
-    
+  
     // print possibleMoves array underneath the board!
     cout << "\nPOSSIBLE MOVES: " << endl;
     for(int i = 0; i < ROWS; i++) {
@@ -177,36 +216,10 @@ void Board::printBoard() {
             cout << ' ' << possibleMoves[i][j] << "  ";
         }
         cout << endl;
-    }
-    
+    }*/
     
 }
 
 Card Board::getCard(int i, int j) {
     return board[i][j];
 }
-
-bool Board::checkPossibleMove(int i, int j) {
-    return possibleMoves[i][j];
-}
-
-bool * Board::getPossibleMoves() {
-    return (bool*)possibleMoves;
-}
-
-bool Board::isPossibleMove() {
-    for(int i = 0; i < ROWS; i++) {
-        for(int j = 0; j < COLS; j++) {
-            if(possibleMoves[i][j] == true) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-
-
-
-
-
--- GameSrc/card.cpp
+++ GameSrc/card.cpp
@@ -5,33 +5,29 @@ using namespace std;
 
 Card::Card() {
     id = -1;            // set id = -1, this card is an instance of open space on the board
-    
-    top = 'o';          // this card is free space
-    top_L = 'o';
-    top_R = 'o';
-    
-    bot = 'o';
-    bot_L = 'o';
-    bot_R = 'o';
-    
-    left = 'o';
-    right = 'o';
-    mid = 'o';
     orientation = 0;
-    
-    top_RoadID = -1;    // not part of any feature
-    bot_RoadID = -1;
-    left_RoadID = -1;
-    right_RoadID = -1;
-    mid_RoadID = -1;
+    isden = 0;
+    endtrail = 0;   
 
-    
+    top = 'n';         // filler cards do not have true tblr values. Sides = "null".
+    bot = 'n';
+    left = 'n';
+    right = 'n';
+/*
+    T_R = 'n';
+    T_L = 'n';
+    B_R = 'n';
+    B_L = 'n';
+    mid = 'n';
+*/
 }
 
-Card::Card(int id) {
-    this->id = id;          // set this cards id
+Card::Card(int inputid) {
+    id = inputid;           // set this cards id
     assignSides(id);        // set default sides corresponding to this card type
     orientation = 0;
+    isden = 0;
+    endtrail = 0;
 }
 
 void Card::rotate() {   // rotate card 90 degrees clockwise
@@ -40,158 +36,83 @@ void Card::rotate() {   // rotate card 90 degrees clockwise
     top = left;
     left = bot;
     bot = temp;
-    
-    temp = top_R;
-    top_R = top_L;
-    top_L = bot_L;
-    bot_L = bot_R;
-    bot_R = temp;
-    
-    orientation = (orientation + 90) % 360;
-    printCard();
+    orientation = (orientation + 90) % 360; 
 }
 
 void Card::printCard() {
     cout << "Card id = " << id << endl;
     // ---------------------------------
-    cout << top_L << " "  << top << " " << top_R << " " << endl;
-    cout << left  << " "  << mid << " " << right << " " << endl;
-    cout << bot_L << " "  << bot << " " << bot_R << " " << endl;
+    cout << " "  << top << " " << endl;
+    cout << left << " " << right << endl;
+    cout << " "  << bot << " " << endl;
 }
 
-void Card::assignSides(int id) {    // completable features: lakes (l), game-trails (t), jungle (j), & dens
+
+// Kurt -probably going to have to change this if he doesn't give us a list of cards
+// Great for right now though, let's keep it
+void Card::assignSides(int id) {
     
     // mark top of card depending on card id
-    if(id == 0 || id == 1  || id == 2 || id == 8 || id == 10 || id == 13 || id == 16 || id == 17) {                        // card with lake top
-        top = 'J';
+    if(id == 17 || id == 2 || id == 1 || 
+    	id == 23 || id == 10 || id == 13) {          				// card with jungle top
+        top = 'j';
     }
-    else if (id == 3 || id == 4 || id == 5 || id == 6 || id == 14 || id == 15 || id == 18 || id == 19 || id == 20
-              || id == 21  || id == 22 || id == 23 || id == 24) {     // card with trail top13
-        top = 'T';
+    else if (id == 7 || id == 16) {                  				// card with game-trail top
+        top = 'g';
     }
-    else {                             // card with jungle top
-        top = 'L';
+    else {                                           				// card with lake top
+        top = 'l';
     }
     
     // mark right of card depending on card id
-    if(id == 3) {          // card with city right
-        right = 'T';
+    if(id == 18 || id == 5 || id == 19 
+    	|| id == 9 || id == 20 || id == 1 || id == 23) {        	// card with lake right
+        right = 'l';
     }
-    else if (id == 0 || id == 1 || id == 2 || id == 4 || id == 5 || id == 6 || id == 12 || id == 25 || id == 26 || id == 11) {                          // card with road right
-        right = 'J';
+
+    else if (id == 15 || id == 22 || id == 14 || id == 0 
+                || id == 3 || id == 13 || id == 16) {               // card with game-trail right
+        right = 'g';
+
     }
-    else {                                                                                      // card with field right
-        right = 'L';
+    else {                                                          // card with jungle right
+        right = 'j';
     }
     
     // mark bot of card depending on card id
-    if(id == 7 || id == 8 || id == 11 || id == 13 || id == 20 || id == 23 || id == 24 ) {                                                           // card with city bot
-        bot = 'L';
+    if(id == 18 || id == 6) {                                       // card with lake bot
+        bot = 'l';
     }
-    else if (id == 2 || id == 3 || id == 4 || id == 6 || id == 16 || id == 17 || id == 18 || id == 19 || id == 21 || id == 22 || id == 25 || id == 26) {                            // card with field bot
-        bot = 'T';
+    else if (id == 17 || id == 5 || id == 19 || id == 12 || id == 21 || id == 1
+             || id == 23 || id == 4 || id == 8 || id == 3) {        // card with jungle bot
+        bot = 'j';
     }
-    else {                                                                              // card with road bot
-        bot = 'J';
+    else {                                                          // card with game-trail bot
+        bot = 'g';
     }
     
     // mark left of card depending on card id
-    if(id == 3 || id == 5 || id == 6 || id == 14 || id == 15 || id == 16 || id == 17 || id == 21 || id == 22 || id == 23 || id == 24) {          // card with city left
-        left = 'T';
+    if(id == 18 || id == 5 || id == 19 || id == 9 
+    	|| id == 20 || id == 12 || id == 21 || id == 15 
+    	|| id == 22 || id == 1 || id == 23 || id == 4) {          	// card with lake left
+        left = 'l';
     }
-    else if (id == 7 || id == 8 || id == 10 || id == 20) {    // card with road left
-        left = 'L';
+
+    else if (id == 11 || id == 0 || id == 3 || id == 10 
+    			|| id == 13 || id == 16) {    						// card with game-trail left
+        left = 'g';
     }
-    else {                                                                              // card with field left
-        left = 'J';
-    }
-   
-   // mark TOP LEFT of card depending on card id
-    if(id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6 ||
-        id == 13 || id == 14 || id == 15 || id == 16 || id == 17 || id == 18 || id == 19
-          || id == 21 || id == 22 || id == 23 || id == 24) {                        //
-        top_L = 'J';
-    }
-    else if (id == 7) {     //
-        top_L = 'L';
-    }
-    else if (id == 8 || id == 9 || id == 10 || id == 11 || id == 12 || id == 20 || id == 25 || id == 26) {     // card with lake/jungle edge (Hybrid)
-        top_L = 'H';
-    }
-    else {                             //
-        top_L = 'o';
+    else {                                                          // card with jungle left
+        left = 'j';
     }
     
-    // mark TOP RIGHT of card depending on card id
-    if(id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6) {                        // card with lake top
-        top_R = 'J';
-    }
-    else if (id == 7 || id == 9) {     // card with trail top13
-        top_R = 'L';
-    }
-    else if (id == 8 || id == 10 || id == 11 || id == 12 || id == 13 || id == 14 || id == 15 || id == 16
-              || id == 17 || id == 18 || id == 19 || id == 20 || id == 21 || id == 22 || id == 23
-              || id == 24 || id == 25 || id == 26) {     // card with lake/jungle edge (Hybrid)
-        top_R = 'H';
-    }
-    else {                             // card with jungle top
-        top_R = 'o';
-    }
-    
-    // mark BOT RIGHT of card depending on card id
-    if(id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6 || id == 12  || id == 25
-            || id == 26) {                        // card with lake top
-        bot_R = 'J';
-    }
-    else if (id == 7 || id == 8 || id == 20|| id == 23|| id == 24) {     // card with trail top13
-        bot_R = 'L';
-    }
-    else if (id == 9 || id == 10 || id == 11 || id == 14 || id == 15 || id == 16 || id == 17 || id == 18
-              || id == 19 || id == 21 || id == 22) {     // card with lake/jungle edge (Hybrid)
-        bot_R = 'H';
-    }
-    else {                             // card with jungle top
-        bot_R = 'o';
-    }
-    
-    // mark BOT LEFT of card depending on card id
-    if(id == 7 || id == 8 || id == 20) {                        // card with lake top
-        bot_L = 'L';
-    }
-    else if (id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6
-             || id == 9 || id == 12 || id == 14|| id == 15|| id == 16|| id == 17|| id == 18|| id == 19
-             || id == 21 || id == 22 || id == 25 || id == 26) {     // card with trail top13
-        bot_L = 'J';
-    }
-    else if (id == 10 || id == 11 || id == 13 || id == 23 || id == 24) {     // card with lake/jungle edge (Hybrid)
-        bot_L = 'H';
-    }
-    else {                             // card with jungle top
-        bot_L = 'o';
-    }
-    
-    
-    // mark MID of card depending on card id
-    if(id == 7 || id == 8 || id == 10 || id == 20) {                        // card with lake top
-        mid = 'L';
-    }
-    else if (id == 3 || id == 4 || id == 5 || id == 6 || id == 14 || id == 15 || id == 16 || id == 17 || id == 18 || id == 19
-             || id == 21 || id == 22 || id == 25 || id == 26 || id == 23 || id == 24) {     // card with trail top13
-        mid = 'T';
-    }
-    else if (id == 0 || id == 10 || id == 11 || id == 12 || id == 13) {     // card with trail top13
-        mid = 'J';
-    }
-    else {                             // card with jungle top
-        mid = 'o';
-    }
-    
-    
-    top_RoadID =   -1;    // not part of any feature
-    bot_RoadID =   -1;
-    left_RoadID =  -1;
-    right_RoadID = -1;
-    mid_RoadID =   -1;
+/*
+    T_R = 'o';
+    T_L = 'o';
+    B_R = 'o';
+    B_L = 'o';
+    mid = 'o';
+*/
 }
 
 char Card::getTop() {
@@ -214,22 +135,24 @@ int Card::getOrient() {
     return orientation;
 }
 
-char Card::getTop_L(){
-    return top_L;
+/*
+char Card::getT_L(){
+    return T_L;
 }
 
-char Card::getTop_R(){
-    return top_R;
+char Card::getT_R(){
+    return T_R;
 }
 
 char Card::getMid(){
     return mid;
 }
 
-char Card::getBot_L(){
-    return bot_L;
+char Card::getB_L(){
+    return B_L;
 }
 
-char Card::getBot_R(){
-    return bot_R;
+char Card::getB_R(){
+    return B_R;
 }
+*/
